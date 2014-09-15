6e3793117377f99c10031a863dca70b6f2ca2638
Emma Sevastian <emma.sevastian@gmail.com>
2014-09-15 04:17:15 +0300

    demo2014

---
+++ src-scenario/scenario/s2014/demo/gui/Demo.java
@@ -0,0 +1,13 @@
+package scenario.s2014.demo.gui;
+
+import java.util.Vector;
+
+import tatami.core.agent.visualization.AgentGui.InputListener;
+
+public class Demo extends javax.swing.JFrame {
+    
+    enum PDAComponents {
+		CLEAR, CONOPINION, JOIN;
+	}
+
+}
---
+++ src-scenario/scenario/s2014/demo/gui/DemoAgentOutput.java
@@ -0,0 +1,61 @@
+package scenario.s2014.demo.gui;
+
+import java.util.Vector;
+
+import tatami.core.agent.visualization.AgentGui.InputListener;
+
+public class DemoAgentOutput extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = -2811079879336761997L;
+
+	public javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTextArea jTextArea1;
+    
+    public DemoAgentOutput() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+
+    	jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(322, 232));
+        setTitle("Attendee ");
+        setBounds(700, 700, 322, 232);
+        
+        jScrollPane1.setAutoscrolls(true);
+        jScrollPane1.setHorizontalScrollBar(null);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(0, 102, 0));
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+            )
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+            )
+        );
+        pack();
+    }
+
+}
