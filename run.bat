0714d6c90c451f5f4043ab383645f486d7b705b7
andreiolaru <cs@andreiolaru.ro>
2015-05-07 12:32:54 +0300

    Improved scripts and added bash version.

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo Use this only if you do not wish to set arguments in BootDefaultArguments and run Boot
-@echo Usage: simulation-file-xml main-host main-port local-host local-port screen-area-width screen-area-height local-container-name
+@echo Usage: run.bat simulation-file-xml main-host main-port local-host local-port screen-area-width screen-area-height local-container-name
 @echo Defaults are: default null -1 null -1 800 600 null
 @echo ===============================================================
 @echo off
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo Use this only if you do not wish to set arguments in BootDefaultArguments and run Boot
+echo Usage: run.sh simulation-file-xml main-host main-port local-host local-port screen-area-width screen-area-height local-container-name
+echo Defaults are: default null -1 null -1 800 600 null
+
+if [ "$1" == "--help" ]; then
+	exit 0
+fi
+if [ "$1" == "-h" ]; then
+	exit 0
+fi
+
+echo ===============================================================
+
+java -Djava.ext.dirs=libs -cp bin tatami.simulation.Boot "$@"
+
+echo ===============================================================
+echo Press any key
+read -n 1 -s
+exit 0
